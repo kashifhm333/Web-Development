6802395cf80ad1dd9bb4a5c80485532db26a06e5
kashifhm333 <Kashifjaanmemon786110@gmail.com>
2026-07-22 13:42:08 +0500

    Intro Next.js

---
+++ Next js/intro/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "@/components/Navbar";
+const page = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>ronaldo is the best player in the world</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ Next js/intro/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+      <h1>I am NavBar</h1>
+    </div>
+  )
+}
+
+export default Navbar
